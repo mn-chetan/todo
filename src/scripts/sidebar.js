839f430868f43b0fea13268d4c327849ae580349
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-03-04 17:15:02 +0530

    Display project modal on add project click
    - Modal contains a form to add a project
    - Form includes 2 buttons for adding and canceling
    - Modal has position fixed and is centered on the screen

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -20,3 +20,70 @@ export const switchProject = () => {
     });
   });
 };
+
+export const addProject = () => {
+  const addProject = document.querySelector(".add-project");
+
+  // Open a modal when the add project button is clicked
+  addProject.addEventListener("click", () => {
+    // Create modal structure
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+
+    const closeButton = document.createElement("span");
+    closeButton.classList.add("close-btn");
+    closeButton.innerHTML = "&times;";
+
+    const h3 = document.createElement("h3");
+    h3.textContent = "Add new project";
+
+    const form = document.createElement("form");
+
+    // Form group for project name input
+    const formGroup = document.createElement("div");
+    formGroup.classList.add("form-group");
+
+    const projectName = document.createElement("label");
+    projectName.classList.add("project-name");
+    projectName.textContent = "Project Name";
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.id = "project-name";
+    input.placeholder = "Enter project name";
+
+    formGroup.appendChild(projectName);
+    formGroup.appendChild(input);
+
+    // Buttons for form actions
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btn-container");
+
+    const cancelButton = document.createElement("button");
+    cancelButton.classList.add("btn", "btn-cancel");
+    cancelButton.type = "button";
+    cancelButton.textContent = "Cancel";
+
+    const submitButton = document.createElement("button");
+    submitButton.classList.add("btn", "btn-add");
+    submitButton.type = "submit";
+    submitButton.textContent = "Add Project";
+
+    btnContainer.appendChild(cancelButton);
+    btnContainer.appendChild(submitButton);
+
+    form.appendChild(formGroup);
+    form.appendChild(btnContainer);
+
+    modalContent.appendChild(closeButton);
+    modalContent.appendChild(h3);
+    modalContent.appendChild(form);
+
+    modal.appendChild(modalContent);
+
+    document.body.appendChild(modal);
+  });
+};
