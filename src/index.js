879d0b127241118dbbd3e481ebfe99464fe73839
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-03-12 15:04:25 +0530

    Import and initialize addTodo function for creating new tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { switchProject, addProject, loadProjects } from "./scripts/sidebar";
+import { addTodo } from "./scripts/todo";
 
 loadProjects();
 switchProject();
 addProject();
+addTodo(); 
